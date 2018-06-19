92b96e38bdd55a4ac7ca4c784dd3b6532ba7a98f
Dmitriy Dubson <ddubson@pivotal.io>
2018-06-19 13:33:19 -0400

    Add negative test paths

--- src/main/kotlin/com/ddubson/xunit/LogAnalyzer.kt
+++ src/main/kotlin/com/ddubson/xunit/LogAnalyzer.kt
@@ -2,7 +2,7 @@ package com.ddubson.xunit
 
 class LogAnalyzer {
     fun isValidLogFileName(fileName: String): Boolean {
-        if(fileName.endsWith(".SLF")) {
+        if(fileName.endsWith(".SLF", ignoreCase = true)) {
             return true
         }
 
--- src/test/kotlin/com/ddubson/xunit/LogAnalyzerTest.kt
+++ src/test/kotlin/com/ddubson/xunit/LogAnalyzerTest.kt
@@ -1,18 +1,43 @@
 package com.ddubson.xunit
 
 import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 internal class LogAnalyzerTest {
+    private var logAnalyzer: LogAnalyzer? = null
+
+    @BeforeEach
+    fun beforeEach() {
+        // Arrange
+        logAnalyzer = LogAnalyzer()
+    }
+
     @Test
     fun isValidLogFileName_givenABadFileName_returnsFalse() {
-        // Arrange
-        val logAnalyzer = LogAnalyzer()
-
         // Act
-        val result = logAnalyzer.isValidLogFileName("fileWithBadExtension.foo")
+        val result = logAnalyzer!!.isValidLogFileName("fileWithBadExtension.foo")
 
         // Assert
         assertFalse(result)
+    }
+
+    @Test
+    fun isValidLogFileName_givenAGoodExtensionLowercase_returnsTrue() {
+        // Act
+        val result = logAnalyzer!!.isValidLogFileName("fileWithGoodExtension.slf")
+
+        // Assert
+        assertTrue(result)
+    }
+
+    @Test
+    fun isValidLogFileName_givenAGoodExtensionUppercase_returnsTrue() {
+        // Act
+        val result = logAnalyzer!!.isValidLogFileName("fileWithGoodExtension.SLF")
+
+        // Assert
+        assertTrue(result)
     }
 }
